61dd9d8f582270305c98e2aa80ce9c168f09c405
Elisa Flemer <elisaflemer@gmail.com>
2023-04-29 20:34:23 -0300

    Globe with text

--- carbonverifier/src/components/BackgroundGlobe.jsx
+++ carbonverifier/src/components/BackgroundGlobe.jsx
@@ -1,9 +1,9 @@
-import planet from '../assets/planet.svg';
+import planet from '../assets/planeta.png';
 
 export default function BackgroundGlobe() {
     return (
         <div class="absolute bottom-0 overflow-hidden w-full">
-            <img src={planet} className="transform translate-y-[80%] w-full" />
+            <img src={planet} className="transform translate-y-[34%] translate-x-4 w-full"/>
         </div>
 
     )
