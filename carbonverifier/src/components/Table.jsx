01e91c83b53a64e374b63347193ef1cf3aa3bc90
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-04-29 20:49:22 -0300

    table model

--- carbonverifier/src/components/Table.jsx
+++ carbonverifier/src/components/Table.jsx
@@ -1,10 +1,57 @@
 import Menu from "../components/Menu";
-
+import GerdauLogo from "../assets/gerdau.png"
 export default function Table() {
     return (
         <div className="h-screen w-full bg-background">
+        
             
-            <h1>lelelelledklasmdklasmdklasdmklasmd</h1>
+            <div className="w-100 h-1/2	 bg-gray-table bg-opacity-87 rounded-md mt-8 ml-8 mr-8 overflow-auto">
+
+                <div className="h-0.5"></div>
+
+
+                <div className="w-auto h-16 bg-gray-table-topic rounded-md my-4 mx-4 container flex flex-row items-center justify-between">
+                    <h1 className='font-montserrat font-bold px-16 text-white' >1º</h1>
+                    <img src={GerdauLogo} alt="Gerdau Logo" className='w-32' />
+                    <h1 className='font-montserrat font-bold px-16 text-white' >Gerdau S.A.</h1>
+                    <h1 className='font-montserrat font-bold px-16 text-white' >999 CVT</h1>
+                </div>
+
+                <div className="w-auto h-16 bg-gray-table-topic rounded-md my-4 mx-4 container flex flex-row items-center justify-between">
+                    <h1 className='font-montserrat font-bold px-16 text-white' >1º</h1>
+                    <img src={GerdauLogo} alt="Gerdau Logo" className='w-32' />
+                    <h1 className='font-montserrat font-bold px-16 text-white' >Gerdau S.A.</h1>
+                    <h1 className='font-montserrat font-bold px-16 text-white' >999 CVT</h1>
+                </div>
+
+                <div className="w-auto h-16 bg-gray-table-topic rounded-md my-4 mx-4 container flex flex-row items-center justify-between">
+                    <h1 className='font-montserrat font-bold px-16 text-white' >1º</h1>
+                    <img src={GerdauLogo} alt="Gerdau Logo" className='w-32' />
+                    <h1 className='font-montserrat font-bold px-16 text-white' >Gerdau S.A.</h1>
+                    <h1 className='font-montserrat font-bold px-16 text-white' >999 CVT</h1>
+                </div>
+
+                <div className="w-auto h-16 bg-gray-table-topic rounded-md my-4 mx-4 container flex flex-row items-center justify-between">
+                    <h1 className='font-montserrat font-bold px-16 text-white' >1º</h1>
+                    <img src={GerdauLogo} alt="Gerdau Logo" className='w-32' />
+                    <h1 className='font-montserrat font-bold px-16 text-white' >Gerdau S.A.</h1>
+                    <h1 className='font-montserrat font-bold px-16 text-white' >999 CVT</h1>
+                </div>
+
+                <div className="w-auto h-16 bg-gray-table-topic rounded-md my-4 mx-4 container flex flex-row items-center justify-between">
+                    <h1 className='font-montserrat font-bold px-16 text-white' >1º</h1>
+                    <img src={GerdauLogo} alt="Gerdau Logo" className='w-32' />
+                    <h1 className='font-montserrat font-bold px-16 text-white' >Gerdau S.A.</h1>
+                    <h1 className='font-montserrat font-bold px-16 text-white' >999 CVT</h1>
+                </div>
+
+                
+                
+                
+
+
+                
+            </div>
         </div>
     )
 }
--- carbonverifier/src/pages/Ranking.jsx
+++ carbonverifier/src/pages/Ranking.jsx
@@ -6,6 +6,7 @@ export default function Ranking() {
     return (
         <div className="h-screen w-full bg-background">
             <Menu/>
+            <p className="pt-32 pl-8 font-montserrat text-white	text-2xl font-bold"> Top CVT Buyers</p>
             <Table />
         </div>
     )
