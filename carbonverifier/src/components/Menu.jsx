fae62ac6847d6cf24a832657771f521507f4fb81
Elisa Flemer <elisaflemer@gmail.com>
2023-04-29 22:41:03 -0300

    Menu working

--- carbonverifier/src/components/Menu.jsx
+++ carbonverifier/src/components/Menu.jsx
@@ -5,6 +5,8 @@ import Home from '../pages/Home';
 import Modal from './Modal';
 import { useContext } from 'react';
 import { WalletContext } from '../App';
+import Ranking from '../pages/Admin';
+import Admin from '../pages/Admin';
 
 export default function Menu() {
     const { isConnected } = useContext(WalletContext);
@@ -14,9 +16,10 @@ export default function Menu() {
         <div className='flex justify-between w-full absolute top-5 font-montserrat items-center text-white font-bold px-5 text-sm'>
             <div className='flex justify-between items-center gap-10'>
                 <img src={logo} alt="Carbon Verifier Logo" className='w-14' />
-                <Link to={Home}>Home</Link>
-                <Link to={Home}>Swap</Link>
-                <Link to={Home}>Top Buyers</Link>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/'}>Swap</Link>
+                <Link to={'/ranking'}>Top Buyers</Link>
+                <Link to={'/admin'}>Admin</Link>
             </div>
             <div className='flex justify-between'>
                 <button className='bg-gradient-to-r from-[#414141] to-[#1f1f1f] 
