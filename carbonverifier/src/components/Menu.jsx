06528910f80fcfee70bfd29fc255b8330081f839
Alberto Miranda <alberto.miranda@sou.inteli.edu.br>
2023-04-30 00:31:17 -0300

    1inch integration

--- carbonverifier/src/components/Menu.jsx
+++ carbonverifier/src/components/Menu.jsx
@@ -1,32 +1,38 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import logo from '../assets/logo.svg';
-import Home from '../pages/Home';
-import Modal from './Modal';
-import { useContext } from 'react';
-import { WalletContext } from '../App';
-import Ranking from '../pages/Admin';
-import Admin from '../pages/Admin';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import logo from "../assets/logo.svg";
+import Home from "../pages/Home";
+import Modal from "./Modal";
+import { useContext } from "react";
+import { WalletContext } from "../App";
+import Ranking from "../pages/Admin";
+import Admin from "../pages/Admin";
 
 export default function Menu() {
-    const { isConnected } = useContext(WalletContext);
-    const [isOpen, setIsOpen] = useState(false);
+  const isConnected =
+    localStorage.getItem("accounts") != null ||
+    useContext(WalletContext).isConnected;
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <div className='flex justify-between w-full absolute top-5 font-montserrat items-center text-white font-bold px-5 text-sm'>
-            <div className='flex justify-between items-center gap-10'>
-                <img src={logo} alt="Carbon Verifier Logo" className='w-14' />
-                <Link to={'/'}>Home</Link>
-                <Link to={'/'}>Swap</Link>
-                <Link to={'/ranking'}>Top Buyers</Link>
-                <Link to={'/admin'}>Admin</Link>
-            </div>
-            <div className='flex justify-between'>
-                <button className='bg-gradient-to-r from-[#414141] to-[#1f1f1f] 
-                text-white py-2 px-4 rounded-md'
-                    onClick={() => setIsOpen(true)}>{isConnected ? 'Connected' : 'Connect wallet'}</button>
-            </div>
-            {isOpen && <Modal setIsOpen={setIsOpen}/>}
-        </div>
-    )
+  return (
+    <div className="flex justify-between w-full absolute top-5 font-montserrat items-center text-white font-bold px-5 text-sm">
+      <div className="flex justify-between items-center gap-10">
+        <img src={logo} alt="Carbon Verifier Logo" className="w-14" />
+        <Link to={"/"}>Home</Link>
+        <Link to={"/"}>Swap</Link>
+        <Link to={"/ranking"}>Top Buyers</Link>
+        <Link to={"/admin"}>Admin</Link>
+      </div>
+      <div className="flex justify-between">
+        <button
+          className="bg-gradient-to-r from-[#414141] to-[#1f1f1f] 
+                text-white py-2 px-4 rounded-md"
+          onClick={() => setIsOpen(true)}
+        >
+          {isConnected ? "Connected" : "Connect wallet"}
+        </button>
+      </div>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
+    </div>
+  );
 }
--- carbonverifier/src/components/Modal.jsx
+++ carbonverifier/src/components/Modal.jsx
@@ -1,44 +1,84 @@
 import { useState, useContext } from "react";
 import { WalletContext } from "../App";
 
-export default function Modal({setIsOpen}) {
-    const { isConnected, setIsConnected, walletAddress, setWalletAddress } = useContext(WalletContext);
+export default function Modal({ setIsOpen }) {
+  const { isConnected, setIsConnected, walletAddress, setWalletAddress } =
+    useContext(WalletContext);
 
-    const connectWallet = async () => {
-        try {
-            if (!ethereum) {
-                alert("Instale uma carteira como Metamask");
-            }
-            const accounts = await ethereum.request({
-                method: 'eth_requestAccounts',
-            });
-            setWalletAddress(accounts[0]);
-            setIsConnected(true);
-            setIsOpen(false)
-        } catch (error) {
-            setIsConnected(false);
-        }
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        alert("Instale uma carteira como Metamask");
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setWalletAddress(accounts[0]);
+      setIsConnected(true);
+      setIsOpen(false);
+    } catch (error) {
+      setIsConnected(false);
     }
+  };
 
-    const connectInch = async () => {
-        window['__1inch_connect_init_rpc__'] = {
-            1: "https://mainnet.infura.io/v3/5851ae72b9fc41228470db23c2c9154d"
-        };
-    }
+  const connectInch = async () => {
+    window["__1inch_connect_init_rpc__"] = {
+      1: "https://mainnet.infura.io/v3/5851ae72b9fc41228470db23c2c9154d",
+    };
+  };
 
-    return (
-        (
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-10 font-montserrat">
-                <div className="bg-background rounded-lg p-6 w-96">
-                    <h2 className="text-lg font-bold mb-4">Connect your wallet...</h2>
-                    <div className='flex flex-col justify-start font-regular'>
-                        <button onClick={connectWallet}>with Metamask</button>
-                        <div onClick={connectInch} className="login-with-1inch-btn">Botão</div>
-                    </div>
-                </div>
+  const [provider, setProvider] = useState(null);
+  const symbol1nch = Symbol("__1inch__");
 
-            </div>
+  const loadAndShow = () => {
+    window["__1inch_connect_init_rpc__"] = {
+      1: "https://mainnet.infura.io/v3/5851ae72b9fc41228470db23c2c9154d",
+    };
 
-        )
-    )
+    const scriptNode = document.createElement("script");
+    scriptNode.setAttribute(
+      "src",
+      "https://cdn.1inch.io/mobile/connect_button/desktop.js"
+    );
+    scriptNode.async = true;
+    scriptNode.onload = () => {};
+    scriptNode.onerror = () => {
+      const load_and_show_qr = document.getElementById("load_and_show_qr");
+      load_and_show_qr.removeAttribute("disabled");
+    };
+    window.addEventListener("1inch_connect_button_init", (e) => {
+      e.detail.provider.enable().then(() => {
+        console.log(
+          "provider",
+          e.detail.provider.chainId,
+          e.detail.provider.accounts
+        );
+        localStorage.setItem("chainId", e.detail.provider.chainId);
+        localStorage.setItem("accounts", e.detail.provider.accounts);
+
+        setProvider(null);
+        window.location.reload();
+      });
+    });
+    document.body.appendChild(scriptNode);
+  };
+
+  return (
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-10 font-montserrat">
+      <div className="bg-background rounded-lg p-6 w-96">
+        <h2 className="text-lg font-bold mb-4">Connect your wallet...</h2>
+        <div className="flex flex-col justify-start font-regular">
+          <button onClick={connectWallet}>with Metamask</button>
+          <div
+            onClick={connectInch}
+            className="flex flex-col justify-start font-regular"
+          >
+            <button id="load_and_show_qr" onClick={loadAndShow}>
+              Connect with 1inch wallet
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
