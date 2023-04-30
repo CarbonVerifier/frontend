ff6ecbe7473a3033e541c78794020f4c5f614849
ViniciosLugli <vinicioslugli@gmail.com>
2023-04-30 03:15:08 -0300

    feat(admin): :sparkles: Integrated admin table with backend events listening

--- carbonverifier/src/components/ItemTableAdmin.jsx
+++ carbonverifier/src/components/ItemTableAdmin.jsx
@@ -1,26 +1,20 @@
-
 export default function ItemTableAdmin(props) {
+	const changeState = (props) => {
+		if (props.payload.includes('exceeded')) {
+			return 'bg-green-600 mx-8 px-16 py-4 rounded-full';
+		} else {
+			return 'bg-red-600 mx-8 px-16 py-4 rounded-full';
+		}
+	};
 
-    const changeState = () => {
-        if (props.condicao == 'ok'){
-            return "bg-green-600 mx-8 px-16 py-4 rounded-full"
-        }   
-        else{
-            return "bg-red-600 mx-8 px-16 py-4 rounded-full"
-        }
-            
-
-    }
-
-    return (
-        <div className="w-auto h-16 bg-gray-table-topic rounded-md my-4 mx-4 container flex flex-row items-center justify-between">
-
-            <img src={props.logo} className='h-12' />
-            <h1 className='font-montserrat font-bold px-16 text-white' >{props.name}</h1>
-            <div className={changeState}></div> 
-
-        </div>            
-
-            
-    )
+	return (
+		<div className="w-100 h-16 bg-gray-table-topic rounded-md my-4 mx-4 container flex flex-row items-center justify-between">
+			<img src={props.logo} className="h-10 pl-5" alt="logo" />
+			<h1 className="font-montserrat font-bold px-16 text-white">{props.name}</h1>
+			<h1 className="font-montserrat font-bold px-16 text-white">{props.date}</h1>
+			<h1 className="font-montserrat font-bold px-16 text-white">{props.address}</h1>
+			<h1 className="font-montserrat font-bold px-16 text-white">{props.payload}</h1>
+			<div className={changeState(props)}></div>
+		</div>
+	);
 }
--- carbonverifier/src/components/TableAdmin.jsx
+++ carbonverifier/src/components/TableAdmin.jsx
@@ -1,15 +1,48 @@
-import ItemTableAdmin from "./ItemTableAdmin"
-import GerdauLogo from "../assets/Gerdau.png"
-export default function TableAdmin() {
-    return (
-        <div className="h-screen w-full bg-background ">
-        
-            
-            <div className="w-100 h-96 bg-gray-table bg-opacity-50 rounded-md mt-8 ml-8 mr-8 overflow-auto relative z-50">
-                <div className="h-0.5"></div>
-                <ItemTableAdmin logo={GerdauLogo} name='Gerdau S.A.' condicao='ok'/>
+import { useEffect, useState } from 'react';
+import ItemTableAdmin from './ItemTableAdmin';
+import GerdauLogo from '../assets/gerdau.png';
+import FaberCastellLogo from '../assets/FaberCastell.png';
+import KlabinLogo from '../assets/Klabin.png';
+import UnileverLogo from '../assets/Unilever.png';
+import ValeLogo from '../assets/Vale.png';
+import axios from 'axios';
 
-            </div>
-        </div>
-    )
+export default function TableAdmin() {
+	const [events, setEvents] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/events')
+			.then((response) => {
+				setEvents(response.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
+	function getLogo(company) {
+		if (company === 'Gerdau') {
+			return GerdauLogo;
+		} else if (company === 'FaberCastell') {
+			return FaberCastellLogo;
+		} else if (company === 'Klabin') {
+			return KlabinLogo;
+		} else if (company === 'Unilever') {
+			return UnileverLogo;
+		} else if (company === 'Vale') {
+			return ValeLogo;
+		}
+	}
+
+	return (
+		<div className="h-screen w-full bg-background w-100">
+			<div className="w-full h-96 bg-gray-table bg-opacity-50 rounded-md mt-8 ml-8 mr-8 overflow-auto relative z-50">
+				<div className="h-0.5"></div>
+				{events.map((event) => (
+					<ItemTableAdmin key={event.contract_address} logo={getLogo(event.company)} name={event.company} address={event.contract_address} payload={event.payload} date={event.date} />
+				))}
+			</div>
+		</div>
+	);
 }
