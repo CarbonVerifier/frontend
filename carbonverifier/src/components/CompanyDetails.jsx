a26d52b88992eca3c8a899d57c087ff53fbe41b7
Elisa Flemer <elisaflemer@gmail.com>
2023-05-14 22:52:08 -0300

    feat(company): completed design

--- carbonverifier/src/components/CompanyDetails.jsx
+++ carbonverifier/src/components/CompanyDetails.jsx
@@ -5,36 +5,44 @@ import toucan from '../assets/toucan.png'
 
 export default function CompanyDetails() {
     const navigate = useNavigate()
-    
+
     return (
         <div className="pt-32 px-10">
             <div className=" bg-gray-table bg-opacity-50 rounded-md w-full flex flex-col p-5">
                 <div className="flex gap-2 justify-between rounded-md">
-                    <div className=" bg-gray-table-topic w-1/2 rounded-md flex flex-col">
+                    <div className=" bg-gray-table-topic w-1/2 rounded-md flex flex-col p-2">
                         <img src={Gerdau}></img>
-                        <h3>Company name: </h3>
-                        <h3>Company ID: </h3>
-                        <h3>Country: </h3>
+                        <h2 className='font-montserrat font-bold text-white'>
+                            Company name: 
+                        </h2>
+                        <h2 className='font-montserrat font-bold text-white'>
+                            Company ID: 
+                        </h2>
+                        <h2 className='font-montserrat font-bold text-white'>
+                            Country: 
+                        </h2>
                     </div>
                     <div className="flex flex-col w-1/2 rounded-md gap-2 justify-between ">
-                        <div className=' bg-gray-table-topic rounded-md h-1/2 '>
-                            CARBON EMITTED
-                            <button className=' bg-gray-table flex gap-2 p-1 rounded-md'>
+                        <div className=' bg-gray-table-topic rounded-md h-1/2 px-2 pt-2 flex flex-col gap-3'>
+                            <h2 className='font-montserrat font-bold text-white text-xl'>Carbon emitted</h2>
+                            <h2 className='font-montserrat font-bold text-white text-2xl self-center'>XX</h2>
+                            <button className=' bg-gray-table flex gap-2 py-2 px-2 rounded-md items-center w-36 justify-center self-center'>
                                 <img src={toucan} className='w-6'></img>
-                                <p>Retire tokens</p>
+                                <p className='text-white'>Retire tokens</p>
                             </button>
                         </div>
-                        <div className=' bg-gray-table-topic rounded-md h-1/2'>
-                            CARBON CREDITS
-                            <button className=' bg-gray-table flex gap-2 p-1 rounded-md' onClick={() => window.open('https://app.ubeswap.org/#/swap', '_blank')}>
+                        <div className=' bg-gray-table-topic rounded-md h-1/2 px-2 pt-2 flex flex-col gap-3'>
+                            <h2 className='font-montserrat font-bold text-white text-xl'>Carbon tokens</h2>
+                            <h2 className='font-montserrat font-bold text-white text-2xl self-center'>XX</h2>
+                            <button className='bg-gray-table flex gap-2 py-2 px-2 rounded-md items-center w-36 justify-center self-center' onClick={() => window.open('https://app.ubeswap.org/#/swap', '_blank')}>
                                 <img src={logo} className='w-7'></img>
-                                <p>Buy tokens</p>
+                                <p className='text-white'>Buy tokens</p>
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        
+
     )
 }
